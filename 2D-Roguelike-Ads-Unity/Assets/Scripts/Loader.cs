d151209b7563a9ff924f3aea1233670350334ace
Solomon Li <solomon@unity3d.com>
2016-09-09 14:30:45 +0800

    no message

---
+++ 2D-Roguelike-Ads-Unity/Assets/Scripts/Loader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour {
+
+	public GameObject gameManager;
+
+	void Awake()
+	{
+		if (GameManager.instance == null)
+			Instantiate (gameManager);
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# 2D-Roguelike-Ads
+# 2D RogueLike
+
+This project is based on unity 2D game tutorial https://unity3d.com/learn/tutorials/projects/2d-roguelike-tutorial
